b5e5308c7d7437d94e7edd0d784ecb215d382dc6
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-07 14:21:31 -0300

    feature/database: product migration

--- database/migrations/2023_09_07_161739_create_products_table.php
+++ database/migrations/2023_09_07_161739_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('price');
+            $table->string('name');
+            $table->string('photo')->nullable();
+            $table->mediumText('description');
+            $table->string('brand');
             $table->timestamps();
         });
     }
