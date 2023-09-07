26f530d8100875de55882c489520f02ec5af1f03
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-07 20:00:32 -0300

    feature/resources: product

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+
+    /**
+     * Customize the pagination information for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array $paginated
+     * @param  array $default
+     * @return array
+     */
+    public function paginationInformation($request, $paginated, $default)
+    {
+        foreach ($default['meta'] as $k => $v) {
+            $default[$k] = $v;
+        }
+        unset($default['meta']);
+
+        return $default;
+    }
+}
