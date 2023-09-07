53ee1f84715343b78bd429150b964f9ccb9703f7
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-07 14:22:26 -0300

    feature/product/controller: move to api folder

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::paginate();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        $validated = $request->validated();
+
+        if ($request->hasFile('photo')) {
+            $validated['photo'] = $request->file('photo')->store('products');
+        }
+
+        return Product::create($validated);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $product)
+    {
+        try {
+            return Product::findOrFail($product);
+        } catch (\Throwable $th) {
+            return response()->json(__('Product Not Found'), 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $validated = $request->validated();
+
+        if ($request->hasFile('photo')) {
+            $validated['photo'] = $request->file('photo')->store('products');
+        }
+
+        $product->update($validated);
+
+        return response()->json(__('Product Updated'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json(status: 204);
+    }
+}
