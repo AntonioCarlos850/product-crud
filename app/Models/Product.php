5a53b447fc91a7904ea5eae7459ebb6984c17fe7
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-07 14:21:50 -0300

    feature/product/model: set accessor and mutator

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn (float $value) => $value/100,
+            set: fn (float $value) => 100 * $value,
+        );
+    }
 }
